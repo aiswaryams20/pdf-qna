e68599c179dbe5697e4756a6e5fd774e3dc539c4
913122104011_AISWARYA MS <121332693+aiswaryams20@users.noreply.github.com>
2025-07-29 13:40:46 +0530

    Removed script file

--- your_main_script.py
+++
@@ -1,7 +0,0 @@
-from utils.groq_engine import get_groq_answer
-
-context = "The mitochondria is the powerhouse of the cell."
-question = "What is mitochondria?"
-
-answer = get_groq_answer(context, question)
-print(answer)
